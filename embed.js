a933af1bcb33ce54c8550a09fc7d7ad3a8670c71
Chen <jonkiky@gmail.com>
2023-04-19 15:56:35 -0400

    update the style of flyer

--- embed.js
+++ embed.js
@@ -2,10 +2,14 @@
         let backgroundColor = document.getElementsByTagName("background-color").length>0?document.getElementsByTagName("background-color")[0].outerText: "#E3CCAE";
         let headerColor =document.getElementsByTagName("header-color").length>0?document.getElementsByTagName("header-color")[0].outerText:"#000000";
         let zipCode = document.getElementsByTagName("zipcode").length>0?document.getElementsByTagName("zipcode")[0].outerText:"20871";
+        let header = document.getElementsByTagName("header").length>0?document.getElementsByTagName("header")[0].outerText:"Missing";
+        let desc = document.getElementsByTagName("desc").length>0?document.getElementsByTagName("desc")[0].outerText:"Have you seen this pet";
             let config = {
                 backgroundColor:backgroundColor,
                 headerColor:headerColor,
-                zipCode:zipCode
+                zipCode:zipCode,
+                header:header,
+                desc:desc
             };
             fetchData(config);
    function fetchData (config) {
@@ -16,8 +20,8 @@
         container.style.backgroundColor =config.backgroundColor;
         html_content= `<a id="missing-pet-link" href="`+data.link+`" target="_blank" style="text-decoration: none;">
       <div style="font-family: Space Grotesk,-apple-system,BlinkMacSystemFont,Athletics,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif; text-align: left;vertical-align: middle; font-weight: 800; margin-bottom: 10px ">
-        <svg viewBox="0 0 72 16">
-          <text x="0" y="15" fill="`+config.headerColor+`">MISSING</text>
+        <svg viewBox="0 0 118 22">
+          <text x="0" y="18" fill="`+config.headerColor+`">`+config.header+`</text>
         </svg>
     </div>
       <div style="margin: 5px; border: 5px solid #fff; position: relative; max-height: 300px; overflow: scroll;" >
@@ -25,7 +29,7 @@
       </div>
       <div style="font-family: Space Grotesk,-apple-system,BlinkMacSystemFont,Athletics,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;text-align: left;vertical-align: middle; font-weight: 800; padding: 5px">
         <svg viewBox="0 0 72 6">
-          <text x="0" y="5" fill="`+config.headerColor+`" font-size="0.3em">HAVE YOU SEEN THIS PET</text>
+          <text x="0" y="5" fill="`+config.headerColor+`" font-size="0.3em">`+config.desc+`</text>
         </svg>
       </div>
       <div id="missing-pet-desc" style="font-family: Space Grotesk,-apple-system,BlinkMacSystemFont,Athletics,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif; color: #fafafa ; text-align: left;vertical-align: middle; font-weight: 600; font-size: 14;padding: 5px">
